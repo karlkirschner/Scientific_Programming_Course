6b1c245a2f2b510c1b9af2dfcd2ec68f46e12b8b
karl <k.n.kirschner@gmail.com>
2020-05-11 11:00:19 +0200

    updated testing lecture

--- assert_example.py
+++ assert_example.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 '''
+assert_example.py
+
 An example for why using assert to test for expected condition is bad.
 
 You can "turn off" asserts by typing "python -O filename.py" and thus
@@ -13,9 +15,9 @@ python -O assert_example.py -> assert is not read and prints a standard error
 '''
 
 def divide_me(number_1=None, number_2=None):
-    assert number_2 != 0, "Error: you can't divide by 0"
     assert number_1 != None, "Error: you did not provide a numerator"
     assert number_2, "Error: you did not provide a denominator"
+    assert number_2 != 0, "Error: you can't divide by 0"
 
     return number_1/number_2
 
