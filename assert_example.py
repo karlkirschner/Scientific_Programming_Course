91f09559f9154c0b7fc4ef5144f4f5a115c070fa
karl <k.n.kirschner@gmail.com>
2020-12-15 10:25:13 +0100

    improved test lecture

--- assert_example.py
+++ assert_example.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+
 '''
 assert_example.py
 
@@ -7,19 +8,18 @@ An example for why using assert to test for expected condition is bad.
 You can "turn off" asserts by typing "python -O filename.py" and thus
     bypassing the check.
 
-Expectations when running the code:
+Expectations when running the code in a bash terminal:
 python assert_example.py -> assert is read and prints out it error
-python -O assert_example.py -> assert is not read and prints a standard error
-
-
+python -O assert_example.py -> assert is not read and program runs
 '''
 
-def divide_me(number_1=None, number_2=None):
+
+def simple_print(number_1=None, number_2=None):
     assert number_1 != None, "Error: you did not provide a numerator"
-    assert number_2, "Error: you did not provide a denominator"
-    assert number_2 != 0, "Error: you can't divide by 0"
+    assert number_2 != None, "Error: you did not provide a denominator"
+    assert number_2 != 0, "Number 2 can't be zero"
 
-    return number_1/number_2
+    print(number_1, number_2)
 
 
-divide_me(number_1=1.0)
+simple_print(number_2=0.0)
