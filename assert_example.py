775eaeeb3f1407fada9434bb566803019cda4688
Karl Kirschner <karl.kirschner@h-brs.de>
2021-06-21 12:17:25 +0200

    updated sci pro and testing

--- assert_example.py
+++ assert_example.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 
 '''
-assert_example.py
-
 An example for why using assert to test for expected condition is bad.
 
 You can "turn off" asserts by typing "python -O filename.py" and thus
@@ -14,7 +12,8 @@ python -O assert_example.py -> assert is not read and program runs
 '''
 
 
-def simple_print(number_1=None, number_2=None):
+def simple_print(number_1: float=None, number_2: float=None) -> float:
+
     assert number_1 != None, "Error: you did not provide a numerator"
     assert number_2 != None, "Error: you did not provide a denominator"
     assert number_2 != 0, "Number 2 can't be zero"
